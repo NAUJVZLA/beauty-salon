bacafb87e9f2d39e4f6a2d472ea29c5ef9f491b5
NAUJVZLA <ju4nc4r1d4d@gmail.com>
2024-11-12 13:31:00 -0500

    hola

--- src/app/infrastucture/services/auth.service.ts
+++ src/app/infrastucture/services/auth.service.ts
@@ -1,19 +1,20 @@
-import { PAuth } from "@/app/core/application/ports";
-import { HttpClient } from "../../../../../../../clonrepoerik/beauty-salon/src/app/infrastructure/utils";
 import { ILoginRequest, ILoginResponse } from "@/app/core/application/dto";
+import { PAuth } from "@/app/core/application/ports/auth.port";
+import { HttpClient } from "../utils";
 
 export class AuthService implements PAuth {
-  private clientHttp: HttpClient;
-  private basePath: string = "auth";
+    private clientHttp: HttpClient
+    private basePath: string = "auth";
 
-  constructor() {
-    this.clientHttp = new HttpClient();
-  }
+    constructor(){
+        this.clientHttp = new HttpClient();
 
-  async login(req: ILoginRequest): Promise<ILoginResponse> {
-    return this.clientHttp.post<ILoginResponse, ILoginRequest>(
-      `${this.basePath}/login`,
-      req
-    );
-  }
+    }
+    
+    async login(req: ILoginRequest): Promise<ILoginResponse> {
+        return this.clientHttp.post<ILoginResponse, ILoginRequest> (
+            `${this.basePath}/login`,
+            req
+        )
+    }
 }
